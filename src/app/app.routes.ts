8cc8c9be4bc71b468743706e2abae0591056b580
tanke <Ruben.Farias.1999@Outlook.es>
2024-01-21 21:35:43 -0600

    Add some validations and new styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        redirectTo: 'sign-up',
+        pathMatch: 'full'
+    },
+    {
+        path: 'sign-up',
+        loadComponent: () => import('./pages/auth/sign-up/sign-up.component').then(c => c.SignUpComponent)
+    }
+];
--- src/app/pages/auth/sign-up/sign-up.component.ts
+++ src/app/pages/auth/sign-up/sign-up.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import { FormControl, FormsModule, ReactiveFormsModule, FormGroup, Validators}  from '@angular/forms';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule,FormsModule],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
 export class SignUpComponent {
+  signUpForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    birthDate: new FormControl('', [Validators.required]),
+    nationality: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required]),
+    confirmPassword: new FormControl('', [Validators.required]),
+  });
+  constructor(private router: Router,) {
+
+  }
+  signUp() {
+    console.log(this.signUpForm.value);
+  }
+  
+  validatePassword() {
+    if (this.signUpForm.value.password != this.signUpForm.value.confirmPassword) {
+      console.log("passwords don't match");
+      this.signUpForm.controls['confirmPassword'].setErrors({ 'incorrect': true });
+    }
+  }
+  goToSignIn() {
+    this.router.navigate(['/sign-in']);
+  }
 
 }
